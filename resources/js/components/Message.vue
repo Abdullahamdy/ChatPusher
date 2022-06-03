bbef393037877fe5b15912cd5f2e04bf42a388e7
abdohamdy1697 <abdohamdy1697@gmail.com>
2022-06-03 18:42:05 +0200

    first commit

---
+++ resources/js/components/Message.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+   <li class="list-group-item" :class="ClassName"><slot></slot></li>
+    <small class="badge badge-primary float-right" :class="badgeClass"> You </small>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:[
+            'color',
+
+        ],
+        computed:{
+            ClassName(){
+                return 'list-group-item-'+this.color
+            },
+             badgeClass(){
+                return 'badge-'+this.color
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Chat App</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+    <style>
+        body{
+            background: #fff;
+        }
+
+        .list-group{
+            overflow-y: scroll;
+            height: 200px;
+        }
+
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="row" id="app">
+            <div class="offset-4 col-4" >
+                <li class="list-group-item active">Chat Room</li>
+                <ul class="list-group" v-chat-scroll>
+                       <message
+                       v-for="value in chat.message"
+                       :key=value.index
+                       color="warning"
+                       >
+                           @{{value}}
+                       </message>
+               </ul>
+               <input type="text" class="form-control" v-model="message" placeholder="Type Your Message here ...." @keyup.enter='send'>
+
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+<script src="{{asset('js/app.js')}}"></script>
+
+</body>
+</html>
